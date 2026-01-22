d046f068bb3fe442bc30c728161487bf236dda7d
EricJK_Briones <129333560+EricJKBriones@users.noreply.github.com>
2026-01-22 14:02:09 +0800

    fix

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"7gl++w3KDKW3MrVKkvE79kv44HtZpope0yBi0rLomog=\"}"
